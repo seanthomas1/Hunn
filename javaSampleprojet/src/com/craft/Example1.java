b23350f4b298597a2f8447e49b8ba56184d030c8
seanthomas1 <63501273+seanthomas1@users.noreply.github.com>
2022-02-18 19:25:45 -0800

    commit form eclips

---
+++ javaSampleprojet/src/com/craft/Example1.java
@@ -0,0 +1,18 @@
+package com.craft;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		System.out.println("Print somethiong");
+		System.out.println("Print somethiong1");
+		System.out.println("Print somethiong2");
+		System.out.println("Print somethiong3");
+		System.out.println("Print somethiong4");
+		System.out.println("Print somethiong5");
+		System.out.println("Print somethiong6");
+		
+	}
+
+}

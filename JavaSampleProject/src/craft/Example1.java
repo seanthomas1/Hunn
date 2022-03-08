5a540f2d6c67fbd863ced9ac9aaf7fc0af2e5b7a
seanthomas1 <seanthomas559@gmail.com>
2022-03-07 21:06:23 -0500

    hello there this is origin to ,,,,,,,,,,

--- JavaSampleProject/src/craft/Example1.java
+++ JavaSampleProject/src/craft/Example1.java
@@ -5,7 +5,7 @@ public class Example1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("print something");
+		System.out.println("print sotlmething");
 		System.out.println("print something");
 		System.out.println("print something");
 		System.out.println("print something");

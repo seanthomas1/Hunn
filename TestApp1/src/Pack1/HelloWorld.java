7525cbe2946c5a971b583930b498038011712e1f
seanthomas1 <63501273+seanthomas1@users.noreply.github.com>
2022-03-13 18:20:31 -0700

    my second commit

---
+++ TestApp1/src/Pack1/HelloWorld.java
@@ -0,0 +1,22 @@
+package Pack1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// This program addds tow given numbers and prints the result on the console
+		
+		int		nunmber1 	 = 25;
+		int  	number2  	 = 50;
+		int result;
+		 result =  nunmber1 +  number2;
+		//print the result
+	System.out.println("Number 1 :"+nunmber1);
+	System.out.println("Number 2 :"+number2);
+	 System.out.println("Result of additon : "+result); 
+		 
+		
+		
+
+	}
+
+}

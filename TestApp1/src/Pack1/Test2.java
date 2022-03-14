e4901a3c84439dcb6f27d6df7d08025f8b31f81c
seanthomas1 <63501273+seanthomas1@users.noreply.github.com>
2022-03-13 18:36:13 -0700

    this is my second test

---
+++ TestApp1/src/Pack1/Test2.java
@@ -0,0 +1,20 @@
+package Pack1;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int		nunmber1 	 = 70;
+		int  	number2  	 = 80;
+		int result;
+		 result =  nunmber1 +  number2;
+		//print the result
+	System.out.println("Number 1 :"+nunmber1);
+	System.out.println("Number 2 :"+number2);
+	 System.out.println("Result of additon : "+result); 
+		
+		
+
+	}
+
+}
